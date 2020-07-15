e5381aa606074decbc707980936d3bf74c67426e
Alex Ngundji <alexngundji@gmail.com>
2020-07-15 04:06:55 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+ class Anagram
+   attr_accessor :new
+   
+   def initialize(new)
+     @new = new
+   end
+   
+   
+   def match (string)
+     string.select do |a|
+       a.split("").sort == new.split("").sort
+ end
+ end
+ end
